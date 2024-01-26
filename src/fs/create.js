c7a85cbc1ddf75d7c955fd56b396d08130b28af9
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-26 20:18:08 +0500

    feat: add create.js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs';
+
+const content = 'I am fresh and young';
+const path = 'src/fs/files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+    fs.stat(path, (extErr = null, stats) => {
+        if (!stats) {
+            fs.writeFile(path, content, intErr => {
+                if (intErr) {
+                    throw new Error(intErr);
+                }
+            });
+        } else {
+            throw new Error('FS operation failed');
+        }
+    }, );
 };
 
 await create();
