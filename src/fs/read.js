35d6e7456ca803cbe03905be70c18ec42b62c652
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-28 18:19:13 +0500

    feat: add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs';
+
+const filePath = 'src/fs/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    fs.stat(filePath, (_statErr, stats) => {
+        if (!stats) {
+            throw new Error('FS operation failed');
+        } else {
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                if (err) {
+                    throw new Error(err);
+                } else {
+                    console.log(data);
+                }
+            });
+        }
+    });
 };
 
 await read();
