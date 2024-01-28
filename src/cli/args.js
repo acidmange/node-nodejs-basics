a4eb93d411df99a35724c2f43efe7e2711ed65c3
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-28 20:16:55 +0500

    feat: add args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argArr = process.argv;
+
+    if (argArr.length > 2) {
+        const Args = argArr.slice(2);
+        const keys = Args.filter((item) => item.startsWith('--'))
+            .map((item) => item.slice(2));
+        const values = Args.filter((item) => !item.startsWith('--'));
+        const result = keys.reduce((acc, item, ind) => {
+            return [...acc, `${item} is ${values[ind]}`];
+        }, [])
+            .join(', ');
+
+        console.log(result);
+    }
 };
 
 parseArgs();
