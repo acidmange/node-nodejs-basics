3ee5d4b7a0f1f4f8c8ce3f214663ae4fefe87722
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-28 19:49:37 +0500

    feat: add env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+    const pattern = 'RSS_';
+    const envObj = process.env;
+    const keys = Object.keys(envObj);
+
+    const filtKeys = keys.filter((key) => key.startsWith(pattern));
+    const filtObj = filtKeys.reduce((acc, key) => {
+        return { ...acc, [key]: envObj[key] };
+    }, {});
+
+    const result = Object.entries(filtObj).map(([key, value]) => {
+        return `${key}=${value}`;
+    })
+
+    console.log(...result);
 };
 
 parseEnv();
