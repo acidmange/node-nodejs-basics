ba4047dfdab79e9c100c928c637773173ef27e6d
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-28 16:18:03 +0500

    feat: add rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import * as fs from 'node:fs';
+
+const oldPath = 'src/fs/files/wrongFilename.txt';
+const newPath = 'src/fs/files/properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+    fs.open(newPath, (noFileErr) => {
+        if (noFileErr) {
+            fs.stat(oldPath, (_noStatErr, stats) => {
+                if (!stats) {
+                    throw new Error('FS operation failed');
+                } else {
+                    fs.rename(oldPath, newPath, (renameErr) => {
+                        if (renameErr) {
+                            throw new Error(renameErr);
+                        }
+                    });
+                }
+            });
+        } else {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await rename();
