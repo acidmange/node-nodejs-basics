8289ee647f82e69137ca286ef93d45b469616fbb
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-28 17:10:15 +0500

    feat: add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import * as fs from 'node:fs';
+
+const filePath = 'src/fs/files/fileToRemove.txt';
+
 const remove = async () => {
-    // Write your code here 
+    fs.stat(filePath, (_err, stats) => {
+        if (!stats) {
+            throw new Error('FS operation failed');
+        } else {
+            fs.rm(filePath, (rmErr) => {
+                if (rmErr) {
+                    throw new Error(rmErr);
+                }
+            });
+        }
+    });
 };
 
 await remove();
