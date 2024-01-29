83c2725375ad0b1cab0c50728da6586eb82407b4
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-29 18:23:53 +0500

    fix: simplify write.js with pipe

--- src/streams/write.js
+++ src/streams/write.js
@@ -5,11 +5,7 @@ const write = async () => {
     const fPath = 'src/streams/files/fileToWrite.txt';
     const writeStream = fs.createWriteStream(fPath);
 
-    process.stdin.on("data", data => {
-        data = data.toString();
-        writeStream.write(data);
-        process.exit();
-    });
+    process.stdin.pipe(writeStream);
 };
 
 await write();
