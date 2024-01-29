aa5e1eca67c4209be4d08eb69f26a935ff7bd1e1
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-29 15:35:52 +0500

    feat: add write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import * as fs from 'node:fs';
+import process from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    const fPath = 'src/streams/files/fileToWrite.txt';
+    const writeStream = fs.createWriteStream(fPath);
+
+    process.stdin.on("data", data => {
+        data = data.toString();
+        writeStream.write(data);
+        process.exit();
+    });
 };
 
 await write();
