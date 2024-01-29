8d02a8b27a2b77bd68a8e12a85b1b1195277cded
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-29 18:44:01 +0500

    feat: add decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import * as fs from 'node:fs';
+import * as zlib from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const path = 'src/zip/files/archive.gz';
+    const newPath = 'src/zip/files/fileToCompress.txt';
+    const rs = fs.createReadStream(path);
+    const ws = fs.createWriteStream(newPath);
+    const unzip = zlib.createUnzip();
+
+    rs.pipe(unzip).pipe(ws);
 };
 
 await decompress();
