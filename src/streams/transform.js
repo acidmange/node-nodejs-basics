b6c4a30d2a1d5427296f25b3c99abcec1214fdf1
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-29 16:12:14 +0500

    feat: add transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import process, { exit, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+          callback(null, chunk.toString().split("").reverse().join("") + '\n\n');
+        },
+      });
+    
+    process.stdin.on("data", data => {
+        data = data.toString();
+    })
+    .pipe(reverse).pipe(stdout);
 };
 
 await transform();
