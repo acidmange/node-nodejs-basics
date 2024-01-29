38276d0f0191229aa2c9db66150f5ceca6ff81b8
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-01-29 14:53:22 +0500

    feat: add read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import * as fs from 'node:fs';
+import process from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+    const fPath = 'src/streams/files/fileToRead.txt';
+    let stream = fs.createReadStream(fPath);
+    const chunks = [];
+
+    stream.on('readable', () => {
+        let chunk;
+        while (null !== (chunk = stream.read())) {
+            chunks.push(chunk);
+        }
+    });
+
+    stream.on('end', () => {
+        const content = chunks.join('');
+        process.stdout.write(content + '\n');
+    });
 };
 
 await read();
